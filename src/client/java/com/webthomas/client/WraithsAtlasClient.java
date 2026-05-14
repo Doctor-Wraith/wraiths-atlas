2232140adf6e0568fc8dbc8e82e9edb8c4b22107
Thomas van Steen <thomas@webthomas.com>
2026-05-14 09:14:46 +0200

    Setup

---
+++ src/client/java/com/webthomas/client/WraithsAtlasClient.java
@@ -0,0 +1,10 @@
+package com.webthomas.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WraithsAtlasClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/webthomas/client/WraithsAtlasDataGenerator.java
@@ -0,0 +1,11 @@
+package com.webthomas.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WraithsAtlasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
